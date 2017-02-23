c619b17e9c82907ba2471f10bbde39ac250940d3
Harry Chang <harry830622@gmail.com>
2017-02-23 23:35:00 +0800

    Improve Q1

--- hw0/Q1.py
+++ hw0/Q1.py
@@ -3,20 +3,18 @@
 import sys
 import numpy as np
 
-matrix_a = []
-with open(sys.argv[1], "r") as matrix_a_file:
-    for line in matrix_a_file:
-        row = [int(s) for s in line.strip("\n").split(",")]
-        matrix_a.append(row)
+def file_to_matrix(file_name):
+    matrix_list = []
+    with open(file_name, "r") as input_file:
+        for line in input_file:
+            row_list = [int(token) for token in line.strip("\n").split(",")]
+            matrix_list.append(row_list)
+    return np.matrix(matrix_list)
 
-matrix_b = []
-with open(sys.argv[2], "r") as matrix_b_file:
-    for line in matrix_b_file:
-        row = [int(s) for s in line.strip("\n").split(",")]
-        matrix_b.append(row)
-
-result_matrix = np.matrix(matrix_a).dot(np.matrix(matrix_b))
-result = [s for sublist in result_matrix.tolist() for s in sublist]
+matrix_a = file_to_matrix(sys.argv[1])
+matrix_b = file_to_matrix(sys.argv[2])
+result_matrix = matrix_a.dot(matrix_b)
+result = [n for row_list in result_matrix.tolist() for n in row_list]
 result.sort()
 
 with open("ans_one.txt", "w") as output_file:
