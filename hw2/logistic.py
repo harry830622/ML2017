de7da5760d73d79bfe08c9c53a7829928441b69f
Harry Chang <harry830622@gmail.com>
2017-04-05 01:40:48 +0800

    HW2: Add validation set

--- hw2/logistic.py
+++ hw2/logistic.py
@@ -8,8 +8,8 @@ import sys
 feature_config = {
     "bias": [1],
     "age": [1, 2],
-    "fnlwgt": [1, 2],
-    "education-num": [1, 2],
+    "fnlwgt": [],
+    "education-num": [],
     "capital-gain": [1, 2],
     "capital-loss": [1, 2],
     "hours-per-week": [1, 2],
@@ -23,22 +23,44 @@ feature_config = {
     "native-country": [0],
 }
 
+
 def sigmoid(z):
     sig = 1 / (1.0 + np.exp(-z))
     return np.clip(sig, 0.00000000000001, 0.99999999999999)
 
-training_x, training_y, testing_x = feature_extractor.extract_features(sys.argv[1], sys.argv[2], feature_config, True)
+
+def err(loss):
+    return np.sum(np.abs(loss))
+
+
+def cross_entropy(y_hat, y):
+    return np.sum(-1 * y_hat.transpose().dot(np.log(sigmoid(y))) - (
+        1 - y_hat).transpose().dot(1 - np.log(sigmoid(y))))
+
+
+training_x, training_y, testing_x = feature_extractor.extract_features(
+    sys.argv[1], sys.argv[2], feature_config, True)
+
+num_validating_data = training_x.shape[0] // 10
+validating_x = training_x[:num_validating_data]
+validating_y = training_y[:num_validating_data]
+training_x = training_x[num_validating_data:]
+training_y = training_y[num_validating_data:]
+num_training_data = training_x.shape[0]
 
 num_features = training_x.shape[1]
-weights = [ 0.0 for _ in range(num_features) ]
+weights = [0.0 for _ in range(num_features)]
 
-num_iterations = 1e3
+# Training parameters
+num_iterations = 1e6
 learning_rate = 1e1
 
+print("========== Training ==========")
 t = 0
-previous_gradient = [ 0.0 for _ in range(num_features) ]
-weights = np.matrix([weights], dtype = np.float64).transpose()
-previous_gradient = np.matrix([previous_gradient], dtype = np.float64).transpose()
+previous_gradient = [0.0 for _ in range(num_features)]
+weights = np.matrix([weights], dtype=np.float64).transpose()
+previous_gradient = np.matrix(
+    [previous_gradient], dtype=np.float64).transpose()
 while t < num_iterations:
     t += 1
     y = sigmoid(training_x.dot(weights))
@@ -47,7 +69,14 @@ while t < num_iterations:
     previous_gradient += np.square(gradient)
     weights -= learning_rate * gradient / np.sqrt(previous_gradient)
     if t % 100 == 0:
-        print("ERR: %f \tL: %f" % (np.sum(np.abs(loss)), np.sum(-1 * training_y.transpose().dot(np.log(sigmoid(y))) - (1 - training_y).transpose().dot(1 - np.log(sigmoid(y))))))
+        print("ERR: %f \tL: %f" % (err(loss) / float(num_training_data),
+                                   cross_entropy(training_y, y)))
+
+print("========== Validating ==========")
+y = sigmoid(validating_x.dot(weights))
+loss = y - validating_y
+print("ERR: %f \tL: %f" % (err(loss) / float(num_validating_data),
+                           cross_entropy(validating_y, y)))
 
 output_file_name = sys.argv[6]
 with open(output_file_name, "w") as output_file:
