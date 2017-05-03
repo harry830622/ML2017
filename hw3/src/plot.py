352d1b83500ed1dc045a0288a62001a155e6831e
Harry Chang <harry830622@gmail.com>
2017-05-03 13:18:53 +0800

    HW3: Saliency map

--- hw3/src/plot.py
+++ hw3/src/plot.py
@@ -45,6 +45,7 @@ training_x_file_name = sys.argv[1]
 training_y_file_name = sys.argv[2]
 model_file_name = sys.argv[3]
 dump_file_name = sys.argv[4]
+prefix = sys.argv[5]
 
 model = load_model(model_file_name)
 
@@ -66,13 +67,19 @@ training_y = np.array(training_y, dtype=np.float64)
 
 training_x /= 255
 
+shuffle_index = np.arange(len(training_x))
+np.random.seed(19940622)
+np.random.shuffle(shuffle_index)
+training_x = training_x[shuffle_index]
+training_y = training_y[shuffle_index]
+
 num_validating_x = training_x.shape[0] // 10
 validating_x = training_x[:num_validating_x]
 validating_y = training_y[:num_validating_x]
 
 model.summary()
 
-plot_model(model, "structure.png")
+plot_model(model, "{}_structure.png".format(prefix))
 
 plt.plot(history['acc'])
 plt.plot(history['val_acc'])
@@ -80,23 +87,22 @@ plt.title('Model Accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('# of epochs')
 plt.legend(['train', 'valid'], loc='lower right')
-plt.savefig("acc.png")
+plt.savefig("{}_acc.png".format(prefix))
 plt.close()
 
+classes = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
 predicted_y = model.predict_classes(validating_x)
 cnf_matrix = confusion_matrix(validating_y, predicted_y)
 plt.figure()
-plot_confusion_matrix(
-    cnf_matrix,
-    classes=[
-        "Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"
-    ])
-plt.savefig("confusion_matrix.png")
+plot_confusion_matrix(cnf_matrix, classes=classes)
+plt.savefig("{}_confusion_matrix.png".format(prefix))
 plt.close()
 
+img_ids = range(200, 210)
+num_imgs = len(img_ids)
+plt.figure(figsize=(8, num_imgs * 2))
 input_img = model.input
-img_ids = [0]
-for i in img_ids:
+for nth, i in enumerate(img_ids):
     y = predicted_y[i]
     target = K.mean(model.output[:, y])
     grads = K.gradients(target, input_img)[0]
@@ -107,20 +113,26 @@ for i in img_ids:
     heatmap = np.abs(heatmap)
     heatmap = heatmap / np.max(heatmap)
 
-    thresh = 0.5
     see = validating_x[i].reshape((48, 48))
-    see[np.where(heatmap <= thresh)] = np.mean(see)
+    thresh = np.mean(see)
+    img = see * 255
+    see[np.where(heatmap <= thresh)] = thresh
+    see *= 255
 
-    plt.figure()
+    print(nth, classes[y])
+
+    plt.subplot(num_imgs, 3, 1 + 3 * nth)
+    plt.imshow(img, cmap="gray")
+
+    plt.subplot(num_imgs, 3, 2 + 3 * nth)
     plt.imshow(heatmap, cmap=plt.cm.jet)
     plt.colorbar()
     plt.tight_layout()
-    plt.savefig("heatmap.png", dpi=100)
-    plt.close()
 
-    plt.figure()
+    plt.subplot(num_imgs, 3, 3 + 3 * nth)
     plt.imshow(see, cmap="gray")
     plt.colorbar()
     plt.tight_layout()
-    plt.savefig("masked_heatmap.png", dpi=100)
-    plt.close()
+
+plt.savefig("{}_heatmap.png".format(prefix))
+plt.close()
