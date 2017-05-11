6df3fbb70ffe32bed6e84f70463b8ccfbfdc74af
Harry Chang <harry830622@gmail.com>
2017-05-11 18:02:28 +0800

    HW4: Reconstruct faces

--- hw4/pca.py
+++ hw4/pca.py
@@ -16,6 +16,13 @@ def pca(a, num_of_pcs):
     return scores, pcs
 
 
+def reconstruct(a, eigen_v):
+    m = np.mean(a, axis=0)
+    projected_a = np.dot(a - m, eigen_v.T)
+    reconstructed_a = m + np.dot(projected_a, eigen_v)
+    return reconstructed_a
+
+
 faces_dir = os.path.join(os.path.expanduser("~"), "Downloads/face")
 bmp_file_names = [
     file_name for file_name in os.listdir(faces_dir)
@@ -42,10 +49,24 @@ for i in range(9):
 plt.savefig("eigen_faces.png")
 plt.close()
 
+faces = []
+for i in range(4):
+    for j in range(25):
+        bmp_file_name = "{}{:02d}.bmp".format(chr(ord("A") + i), j)
+        face_img = Image.open(os.path.join(faces_dir, bmp_file_name))
+        face = np.array(face_img).flatten()
+        faces.append(face)
+faces = np.array(faces)
+
+reconstructed_faces = reconstruct(faces, eigen_faces[:5])
+
 plt.figure()
-face_img = Image.open(os.path.join(faces_dir, "A10.bmp"))
-face = np.array(face_img).flatten()
-plt.axis("off")
-plt.imshow(face.reshape(64, 64), cmap="gray")
+for i in range(100):
+    plt.subplot(10, 20, i * 2 + 1)
+    plt.axis("off")
+    plt.imshow(faces[i].reshape(64, 64), cmap="gray")
+    plt.subplot(10, 20, i * 2 + 2)
+    plt.axis("off")
+    plt.imshow(reconstructed_faces[i].reshape(64, 64), cmap="gray")
 plt.savefig("reconstructed_faces.png")
 plt.close()
