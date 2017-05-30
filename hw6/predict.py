8058284b2302d93a4bbebc812326b3d6f1b5eea9
Harry Chang <harry830622@gmail.com>
2017-05-31 03:10:50 +0800

    HW6: Bagging (Kaggle 0.84706)

--- hw6/predict.py
+++ hw6/predict.py
@@ -2,7 +2,7 @@
 
 import mf
 
-from config import SEED, VALIDATION_RATIO
+from config import SEED, NUM_MODELS, VALIDATION_RATIO
 from config import NUM_USERS, NUM_MOVIES, LATENT_DIMENSION
 from config import IS_NORMALIZED, IS_REGULARIZED, LAMBDA, IS_BIASED
 from extract import extract_x_test
@@ -15,7 +15,7 @@ import pickle
 
 pwd = sys.argv[1]
 output_file_name = sys.argv[2]
-model_file_name = sys.argv[3]
+suffix = sys.argv[3]
 
 testing_file_name = os.path.join(pwd, "test.csv")
 users_file_name = os.path.join(pwd, "users.csv")
@@ -32,12 +32,18 @@ model = mf.build(
     is_biased=IS_BIASED)
 model.summary()
 
-model.load_weights(model_file_name)
-
 x_test = extract_x_test(testing_file_name, is_biased=IS_BIASED)
 
-ratings = model.predict(
-    np.hsplit(x_test, x_test.shape[1]), batch_size=512, verbose=1)
+all_ratings = []
+for i in range(NUM_MODELS):
+    model_file_name = os.path.join(pwd, "model_{}_{:d}.h5".format(suffix, i))
+
+    model.load_weights(model_file_name)
+
+    all_ratings.append(
+        model.predict(
+            np.hsplit(x_test, x_test.shape[1]), batch_size=512, verbose=1))
+all_ratings = np.array(all_ratings)
 
 if IS_NORMALIZED:
     y_mean_file_name = sys.argv[4]
@@ -45,11 +51,15 @@ if IS_NORMALIZED:
         m = pickle.load(y_mean_file)
         y_mean = m["y_mean"]
         y_mean_index_by_movie_id = m["y_mean_index_by_movie_id"]
-    for i, x in enumerate(x_test):
-        movie_id = x[1]
-        if movie_id in y_mean_index_by_movie_id:
-            ratings[i] += y_mean[y_mean_index_by_movie_id[movie_id]]
-    ratings[ratings < 0] = 0
+
+    for i in range(all_ratings.shape[0]):
+        for j, x in enumerate(x_test):
+            movie_id = x[1]
+            if movie_id in y_mean_index_by_movie_id:
+                all_ratings[i][j] += y_mean[y_mean_index_by_movie_id[movie_id]]
+        all_ratings[i][all_ratings[i] < 0] = 0
+
+ratings = np.mean(all_ratings, axis=0)
 
 with open(output_file_name, "w") as output_file:
     output_file.write("TestDataID,Rating\n")
--- hw6/train.py
+++ hw6/train.py
@@ -2,7 +2,7 @@
 
 import mf
 
-from config import SEED, VALIDATION_RATIO
+from config import SEED, NUM_MODELS, VALIDATION_RATIO
 from config import NUM_USERS, NUM_MOVIES, LATENT_DIMENSION
 from config import IS_NORMALIZED, IS_REGULARIZED, LAMBDA, IS_BIASED
 from extract import extract_xy_train, extract_users
@@ -30,21 +30,10 @@ if __name__ == "__main__":
     training_file_name = os.path.join(pwd, "train.csv")
     users_file_name = os.path.join(pwd, "users.csv")
     movies_file_name = os.path.join(pwd, "movies.csv")
-    model_file_name = os.path.join(pwd, "model_{}.h5".format(suffix))
-    history_file_name = os.path.join(pwd, "history_{}.p".format(suffix))
 
     x_train, y_train = extract_xy_train(
         training_file_name, is_normalized=IS_NORMALIZED, is_biased=IS_BIASED)
     # users = extract_users(users_file_name, num_users=NUM_USERS)
-
-    model = mf.build(
-        num_users=NUM_USERS,
-        num_movies=NUM_MOVIES,
-        latent_dimension=LATENT_DIMENSION,
-        is_regularized=IS_REGULARIZED,
-        lamda=LAMBDA,
-        is_biased=IS_BIASED)
-    model.summary()
 
     num_x_train = x_train.shape[0]
     indices = np.arange(num_x_train)
@@ -56,30 +45,55 @@ if __name__ == "__main__":
     splited_y_train = y_train[:-num_x_valid]
     splited_x_valid = x_train[-num_x_valid:]
     splited_y_valid = y_train[-num_x_valid:]
-    history = model.fit(
-        np.hsplit(splited_x_train, x_train.shape[1]),
-        splited_y_train,
-        validation_data=(np.hsplit(splited_x_valid, x_train.shape[1]),
-                         splited_y_valid),
-        batch_size=512,
-        epochs=100,
-        callbacks=[
-            EarlyStopping(
-                monitor="val_mean_squared_error", patience=3, verbose=1),
-            ModelCheckpoint(
-                model_file_name,
-                save_best_only=True,
-                save_weights_only=True,
-                monitor="val_mean_squared_error",
-                verbose=1),
-        ])
 
-    model.load_weights(model_file_name)
-    rmse_valid = np.sqrt(
-        model.evaluate(
-            np.hsplit(splited_x_valid, x_train.shape[1]), splited_y_valid)[1])
-    print("\n\n\033[1m\033[32mRMSE on validation set: {:f}\033[0m\n".format(
-        rmse_valid))
+    num_splited_x_train = splited_x_train.shape[0]
+    indices = np.arange(num_splited_x_train)
+    for i in range(NUM_MODELS):
+        model_file_name = os.path.join(pwd, "model_{}_{:d}.h5".format(
+            suffix, i))
+        history_file_name = os.path.join(pwd, "history_{}_{:d}.p".format(
+            suffix, i))
 
-    with open(history_file_name, "wb") as history_file:
-        pickle.dump(history.history, history_file)
+        model = mf.build(
+            num_users=NUM_USERS,
+            num_movies=NUM_MOVIES,
+            latent_dimension=LATENT_DIMENSION,
+            is_regularized=IS_REGULARIZED,
+            lamda=LAMBDA,
+            is_biased=IS_BIASED)
+        model.summary()
+
+        np.random.shuffle(indices)
+        splited_x_train = splited_x_train[indices]
+        splited_y_train = splited_y_train[indices]
+        index = int(num_splited_x_train * 0.8)
+
+        history = model.fit(
+            np.hsplit(splited_x_train[:index], x_train.shape[1]),
+            splited_y_train[:index],
+            validation_data=(np.hsplit(splited_x_train[index:],
+                                       x_train.shape[1]),
+                             splited_y_train[index:]),
+            batch_size=512,
+            epochs=100,
+            callbacks=[
+                EarlyStopping(
+                    monitor="val_mean_squared_error", patience=3, verbose=1),
+                ModelCheckpoint(
+                    model_file_name,
+                    save_best_only=True,
+                    save_weights_only=True,
+                    monitor="val_mean_squared_error",
+                    verbose=1),
+            ])
+
+        with open(history_file_name, "wb") as history_file:
+            pickle.dump(history.history, history_file)
+
+        model.load_weights(model_file_name)
+        rmse_valid = np.sqrt(
+            model.evaluate(
+                np.hsplit(splited_x_valid, x_train.shape[1]), splited_y_valid)[
+                    1])
+        print("\n\n\033[1m\033[32mRMSE on validation set: {:f}\033[0m\n".
+              format(rmse_valid))
