58549473727e048d110115a15e0c9e8c81bec503
Harry Chang <harry830622@gmail.com>
2017-06-04 18:24:44 +0800

    HW6: Name layers

--- hw6/mf.py
+++ hw6/mf.py
@@ -14,64 +14,62 @@ def build(num_users=7000,
           is_biased=True,
           **kwargs):
     lamda = kwargs["lamda"] if "lamda" in kwargs else 1e-5
-    users = kwargs["users"] if "users" in kwargs else np.empty((0, 0))
 
-    user_id_input = Input(shape=(1, ))
-    movie_id_input = Input(shape=(1, ))
+    user_id_input = Input(shape=(1, ), name="UserID")
+    movie_id_input = Input(shape=(1, ), name="MovieID")
+
     user_latent = Embedding(
         num_users,
         latent_dimension,
         input_length=1,
         embeddings_initializer="random_uniform",
-        embeddings_regularizer=l2(lamda)
-        if is_regularized else None)(user_id_input)
+        embeddings_regularizer=l2(lamda) if is_regularized else None,
+        name="UserLatent")(user_id_input)
     movie_latent = Embedding(
         num_movies,
-        latent_dimension + users.shape[1],
+        latent_dimension,
         input_length=1,
         embeddings_initializer="random_uniform",
-        embeddings_regularizer=l2(lamda)
-        if is_regularized else None)(movie_id_input)
+        embeddings_regularizer=l2(lamda) if is_regularized else None,
+        name="MovieLatent")(movie_id_input)
 
-    if users.shape[0] != 0:
-        user_latent = Concatenate()([
-            user_latent,
-            Embedding(
-                num_users,
-                users.shape[1],
-                input_length=1,
-                weights=[users],
-                trainable=False)(user_id_input)
-        ])
+    user_latent = Flatten(name="FlattenedUserLatent")(user_latent)
+    movie_latent = Flatten(name="FlattenedMovieLatent")(movie_latent)
 
-    user_latent = Flatten()(user_latent)
-    movie_latent = Flatten()(movie_latent)
-    output = Dot(1)([user_latent, movie_latent])
+    output = Dot(1, name="NoBiasedRating")([user_latent, movie_latent])
+
     model = Model([user_id_input, movie_id_input], output)
 
     if is_biased:
-        bias_input = Input(shape=(1, ))
+        bias_input = Input(shape=(1, ), name="GlobalBiasCoef")
+
         bias = Embedding(
-            1, 1, input_length=1,
-            embeddings_initializer="random_uniform")(bias_input)
+            1,
+            1,
+            input_length=1,
+            embeddings_initializer="random_uniform",
+            name="GlobalBias")(bias_input)
         user_bias = Embedding(
             num_users,
             1,
             input_length=1,
             embeddings_initializer="random_uniform",
-            embeddings_regularizer=l2(lamda)
-            if is_regularized else None)(user_id_input)
+            embeddings_regularizer=l2(lamda) if is_regularized else None,
+            name="UserBias")(user_id_input)
         movie_bias = Embedding(
             num_movies,
             1,
             input_length=1,
             embeddings_initializer="random_uniform",
-            embeddings_regularizer=l2(lamda)
-            if is_regularized else None)(movie_id_input)
-        bias = Flatten()(bias)
-        user_bias = Flatten()(user_bias)
-        movie_bias = Flatten()(movie_bias)
-        output = Add()([output, bias, user_bias, movie_bias])
+            embeddings_regularizer=l2(lamda) if is_regularized else None,
+            name="MovieBias")(movie_id_input)
+
+        bias = Flatten(name="FlattenedGlobalBias")(bias)
+        user_bias = Flatten(name="FlattenedUserBias")(user_bias)
+        movie_bias = Flatten(name="FlattenedMovieBias")(movie_bias)
+
+        output = Add(name="Rating")([output, bias, user_bias, movie_bias])
+
         model = Model([user_id_input, movie_id_input, bias_input], output)
 
     model.compile(optimizer="adam", loss="mse", metrics=["mse"])
